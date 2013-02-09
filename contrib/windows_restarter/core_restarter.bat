9ec7d72251e419e2e87c01b73868af17014c1e59
blizzlikegroup <correio13@gmail.com>
2013-02-09 20:33:28 -0200

    Del useless files.

--- contrib/windows_restarter/core_restarter.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-@title BlizzLikeCore
-CLS
-ECHO Initializing Core (World-Server)...
-:1
-start "BlizzLike Core" /B /MIN /WAIT worldserver.exe -c worldserver.conf
-if %errorlevel% == 0 goto end
-goto 1
-:end
--- contrib/windows_restarter/realm_restarter.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-@title BlizzLikeAuth
-CLS
-ECHO Initializing Realm (Logon-Server)...
-:1
-start "BlizzLike Realm" /B /MIN /WAIT authserver.exe -c authserver.conf
-if %errorlevel% == 0 goto end
-goto 1
-:end
