6e7bfceb94c910380e33993e94e12aed006f3269
blizzlikegroup <correio13@gmail.com>
2013-03-16 12:43:22 -0300

    DB: Upd.

--- src/worldserver/Master.cpp
+++ src/worldserver/Master.cpp
@@ -37,7 +37,7 @@
 
 // Format is BDB_YYYYMMDD
 #ifndef _REQ_BDB_VERSION
-# define _REQ_BDB_VERSION  "BDB_20130312"
+# define _REQ_BDB_VERSION  "BDB_20130316"
 #endif //_REQ_BDB_VERSION
 
 #ifdef _WIN32
